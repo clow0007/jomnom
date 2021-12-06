172627b24d85bfa7c7c0624dc9efac7adb1b354e
JulianYH <jyon0017@student.monash.edu>
2021-12-06 22:15:18 +0800

    initial component generate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
+import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
+import { RestaurantCardComponent } from './restaurant-list/restaurant-card/restaurant-card.component';
+import { CategoryListComponent } from './category-list/category-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MapComponent,
+    RestaurantListComponent,
+    RestaurantCardComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule,
