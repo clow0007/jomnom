830fefeb1a757fed894234745768202d21a26685
Joshua <clow0007@student.monash.edu>
2021-12-07 01:37:08 +0800

    Initial update of category component

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Category } from '../category';
+import { CATEGORIES } from '../mock-category';
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent implements OnInit {
+  // categories: Category[] = [];
+  categories = CATEGORIES;
 
   constructor() { }
 
---
+++ src/app/mock-category.ts
@@ -0,0 +1,10 @@
+import { Category } from "./category";
+
+export const CATEGORIES: Category[] = [
+  { name: 'Western' },
+  { name: 'Malay' },
+  { name: 'Indian ' },
+  { name: 'Chinese' },
+  { name: 'Mamak' },
+  { name: 'Japanese' }
+]
