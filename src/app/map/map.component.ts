f998cba9b13e9d60fa51f6a911c4aee69f831239
ptan0021 <ptan0021@student.monash.edu>
2021-12-07 21:24:16 +0800

    Current location on map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -9,16 +9,66 @@ import {} from 'googlemaps';
 export class MapComponent implements OnInit {
   @ViewChild('map', {static: true}) mapElement: any;
   map: google.maps.Map;
+  infoWindow: google.maps.InfoWindow
 
   constructor() { }
 
   ngOnInit(): void {
-    const mapProperties = {
-      center: new google.maps.LatLng(35.2271, -80.8431),
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+    this.initMap();
+  }
+
+  initMap(): void {
+    this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 6,
+    });
+    this.infoWindow = new google.maps.InfoWindow();
+  
+    const locationButton = document.createElement("button");
+  
+    locationButton.textContent = "Pan to Current Location";
+    locationButton.classList.add("custom-map-control-button");
+  
+    this.map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  
+    locationButton.addEventListener("click", () => {
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position: GeolocationPosition) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+  
+            this.infoWindow.setPosition(pos);
+            this.infoWindow.setContent("Location found.");
+            this.infoWindow.open(this.map);
+            this.map.setCenter(pos);
+          },
+          () => {
+            this.handleLocationError(true, this.infoWindow, this.map.getCenter()!);
+          }
+        );
+      } else {
+        // Browser doesn't support Geolocation
+        this.handleLocationError(false, this.infoWindow, this.map.getCenter()!);
+      }
+    });
+  }
+
+  handleLocationError(
+    browserHasGeolocation: boolean,
+    infoWindow: google.maps.InfoWindow,
+    pos: google.maps.LatLng
+  ) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(this.map);
   }
 
 }
