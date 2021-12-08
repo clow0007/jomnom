dfa2875cffa077fd9dd475b4d34e6524ff96ec44
Joshua <clow0007@student.monash.edu>
2021-12-08 21:07:56 +0800

    Auto stash before merge of "main" and "origin/main"

--- src/app/mock-data/mock-restaurants.ts
+++ src/app/mock-data/mock-restaurants.ts
@@ -1,13 +1,11 @@
 import { Restaurant } from "../interface/restaurant";
 
-var img = new Image();
 export const RESTAURANTS: Restaurant[] = [
-  { name: 'Restaurant Horse', category: 'Dine in • Take away', rating: 5, cuisine: ["Others"] },
-  { name: 'Restoran Hero', category: 'Dine in', rating: 2, cuisine: ["Western", "Other"] },
-  { name: 'Restaurant Kandar', category: 'Dine in • Take away • Delivery', rating: 2, cuisine: ["Indian", "Mamak"] },
-  { name: 'McDonalds', category: 'Dine in • Take away • Delivery', rating: 3, cuisine: ["Western", "Fast-Food"] },
-  { name: 'Restoran Mey Wey', category: 'Dine in • Take away', rating: 4, cuisine: ["Chinese"] },
-  { name: 'De Western Rangers', category: 'Dine in', rating: 2, cuisine: ["Western"] },
-  { name: 'Kedai Mamak Husin', category: 'Dine in • Take away • Delivery', rating: 2, cuisine: ["Mamak"] },
-  // image: img},
+  { name: 'Restaurant Horse', category: 'Dine in • Take away', rating: 5, cuisine: ["Others"], image: "../../../assets/images/horse.jpg" },
+  { name: 'Restoran Hero', category: 'Dine in', rating: 2, cuisine: ["Western", "Other"], image: "../../../assets/images/restaurant_1.jpg"},
+  { name: 'Restaurant Kandar', category: 'Dine in • Take away • Delivery', rating: 2, cuisine: ["Indian", "Mamak"], image: "../../../assets/images/kandar.jpg" },
+  { name: 'McDonalds', category: 'Dine in • Take away • Delivery', rating: 3, cuisine: ["Western", "Fast-Food"], image: "../../../assets/images/mekdi.jpg"},
+  { name: 'Restoran Mey Wey', category: 'Dine in • Take away', rating: 4, cuisine: ["Chinese"], image: "../../../assets/images/meywey.jpg" },
+  { name: 'De Western Rangers', category: 'Dine in', rating: 2, cuisine: ["Western"], image: "../../../assets/images/dewesternrangers.jpg" },
+  { name: 'Kedai Mamak Husin', category: 'Dine in • Take away • Delivery', rating: 2, cuisine: ["Mamak"], image: "../../../assets/images/mamakhusin.jpg" },
 ]
