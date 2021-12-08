9b9758618cc482e5370d2d94e88dc87aa6a21d88
Joshua <clow0007@student.monash.edu>
2021-12-08 14:12:22 +0800

    Corrected category data

--- src/app/mock-data/mock-category.ts
+++ src/app/mock-data/mock-category.ts
@@ -3,10 +3,11 @@ import { Category } from "../interface/category";
 export const CATEGORIES: Category[] = [
   { name: 'Western' },
   { name: 'Malay' },
-  { name: 'Indian ' },
+  { name: 'Indian' },
   { name: 'Chinese' },
   { name: 'Mamak' },
   { name: 'Japanese' },
   { name: 'Korean' },
-  { name: 'Arabic' }
+  { name: 'Arabic' },
+  { name: 'Other'}
 ]
