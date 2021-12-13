7a27210a21b5ca36e758dc8c90fc65214cf4655f
zyon0005 <zyon0005@student.monash.edu>
2021-12-13 21:46:02 +0800

    food challenge

--- src/app/food-challenge/food-challenge.component.ts
+++ src/app/food-challenge/food-challenge.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Restaurant } from '../interface/restaurant';
+import { RestaurantService } from '../services/restaurant.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-food-challenge',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./food-challenge.component.css']
 })
 export class FoodChallengeComponent implements OnInit {
+  restaurants: Restaurant[] = [];
 
-  constructor() { }
+  constructor(private restaurantService: RestaurantService) { }
 
   ngOnInit(): void {
+    this.getRestaurants();
   }
+
+  getRestaurants(): void {
+    this.restaurantService.getRestaurants()
+      .subscribe(restaurants => this.restaurants = restaurants);
+  }
+
+  chooseChallenge(): void{
+    Swal.fire({
+      title: '<strong>Food Challenge Detail</strong>',
+      icon: 'info',
+      html:
+        '<p><b>Collect 10 stamps to get 100 reward points\n </b></p> <p>*Spend RM20 to get one stamp</p>',
+      showCloseButton: true,
+      showCancelButton: false,
+      focusConfirm: false,
+      confirmButtonText:
+        'Accept',
+      // confirmButtonAriaLabel: 'Thumbs up, great!',
+      // cancelButtonText:
+      //   'Decline',
+      // cancelButtonAriaLabel: 'Thumbs down'
+    })
+    
+  }
+
+
 
 }
